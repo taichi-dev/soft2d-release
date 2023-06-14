ff8b738040aa8969301d949fe37c61378cf27ee6
yucrazing <723284893@qq.com>
2023-06-14 14:58:38 +0800

    Add force_field example

---
+++ examples/force_field.cpp
@@ -0,0 +1,175 @@
+// avoid clang-format disorders headers
+// clang-format off
+#include <taichi/taichi.h>
+#include <soft2d/soft2d.h>
+#include "common.h"
+#include "globals.h"
+#include "taichi/aot_demo/framework.hpp"
+#include "emitter.h"
+// clang-format on
+
+using namespace ti::aot_demo;
+using namespace std;
+
+constexpr int win_width = 800;
+constexpr int win_height = 800;
+constexpr float win_fov = 1.0 * win_width / win_height;
+
+struct ForceField : public App {
+
+  S2World world;
+  Emitter emitter;
+
+  std::unique_ptr<GraphicsTask> draw_points;
+  std::unique_ptr<GraphicsTask> draw_collider_texture;
+
+  ti::NdArray<float> x_;
+  ti::Texture collider_texture_;
+
+  virtual AppConfig cfg() const override final {
+    AppConfig out{};
+    out.app_name = "Soft2D";
+    out.framebuffer_width = win_width;
+    out.framebuffer_height = win_height;
+    return out;
+  }
+
+  virtual void initialize() override final {
+
+    // Renderer initialization
+    Renderer &renderer = F.renderer();
+    renderer.set_framebuffer_size(win_width, win_height);
+
+    GraphicsRuntime &runtime = F.runtime();
+
+    x_ = runtime.allocate_vertex_buffer(
+        default_world_config.max_allowed_particle_num, 2);
+
+    collider_texture_ =
+        runtime.allocate_texture2d(default_world_config.grid_resolution *
+                                       default_world_config.fine_grid_scale,
+                                   default_world_config.grid_resolution *
+                                       default_world_config.fine_grid_scale,
+                                   TI_FORMAT_R32F, TI_NULL_HANDLE);
+
+    draw_points = runtime.draw_points(x_)
+                      .point_size(3.0f)
+                      .color(glm::vec3(1, 0.5, 0))
+                      .build();
+    draw_collider_texture = runtime.draw_texture(collider_texture_)
+                                .is_single_channel()
+                                .color(glm::vec3(0.2, 0.8, 0.0))
+                                .build();
+
+    // Soft2D initialization
+    // Create a world
+    S2WorldConfig config = default_world_config;
+    config.enable_debugging = true;
+    world = s2_create_world(TiArch::TI_ARCH_VULKAN, runtime, &config);
+
+    S2Shape shape = make_box_shape(vec2(0.05f, 0.05f));
+
+    S2Material material;
+    material.type = S2MaterialType::S2_MATERIAL_TYPE_FLUID;
+    material.density = 1000.0f;
+    material.youngs_modulus = 1.0f;
+    material.poissons_ratio = 0.2f;
+
+    S2Kinematics kinematics{};
+
+    kinematics.center = vec2(0.5f, 0.5f);
+    kinematics.linear_velocity = vec2(0.0f, 0.0f);
+    kinematics.mobility = S2Mobility::S2_MOBILITY_DYNAMIC;
+
+    emitter = Emitter(world, material, kinematics, shape);
+    emitter.SetFrequency(20);
+
+    // Add the boundary
+    // bottom
+    create_collider(world, make_kinematics({0.5f, 0.0f}),
+                    make_box_shape(vec2(0.5f, 0.01f)));
+    // top
+    create_collider(world, make_kinematics({0.5f, 1.0f}),
+                    make_box_shape(vec2(0.5f, 0.01f)));
+    // left
+    create_collider(world, make_kinematics({0.0f, 0.5f}),
+                    make_box_shape(vec2(0.01f, 0.5f)));
+    // right
+    create_collider(world, make_kinematics({1.0f, 0.5f}),
+                    make_box_shape(vec2(0.01f, 0.5f)));
+  }
+  int frame = 0;
+  virtual void handle_window_event(GLFWwindow *window) override final {
+    static bool on_drag = false;
+    static double last_xpos;
+    static double last_ypos;
+
+    float speed = 50.0f;
+    float scale = 0.005f;
+    int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+
+    if (state == GLFW_PRESS) {
+
+      double xpos, ypos;
+      glfwGetCursorPos(window, &xpos, &ypos);
+      ypos = win_height - 1 - ypos;
+      xpos /= win_width;
+      ypos /= win_height;
+
+      if (on_drag) {
+
+      } else {
+        last_xpos = xpos;
+        last_ypos = ypos;
+        on_drag = true;
+      }
+
+      glm::vec2 force =
+          (glm::vec2(xpos, ypos) - glm::vec2(last_xpos, last_ypos)) * 2.2f *
+          300.0f * 0.004f;
+      // world->AddForceField(force, step_dt, Vec2(xpos, ypos), 0.04f);
+      S2Vec2 s_force = {force.x, force.y};
+      S2Vec2 s_center = {xpos, ypos};
+      s2_apply_impulse_in_circular_area(world, &s_force, &s_center, 0.04f);
+      last_xpos = xpos;
+      last_ypos = ypos;
+    } else if (state == GLFW_RELEASE) {
+      on_drag = false;
+    }
+  }
+  virtual bool update() override final {
+    GraphicsRuntime &runtime = F.runtime();
+
+    emitter.Update(frame);
+    s2_step(world, 0.004);
+
+    // Export particle position data to the external buffer
+    TiNdArray particle_x;
+    s2_get_buffer(world, S2_BUFFER_NAME_PARTICLE_POSITION, &particle_x);
+    ndarray_data_copy(runtime.runtime(), x_.ndarray(), particle_x,
+                      sizeof(float) * 2 *
+                          default_world_config.max_allowed_particle_num);
+
+    // Export collider buffer to texture
+    auto texture = collider_texture_.texture();
+    s2_export_buffer_to_texture(world, S2_BUFFER_NAME_FINE_GRID_COLLIDER_NUM,
+                                true, 0.8f, &texture);
+
+    // Since taichi and renderer use different command buffers, we must
+    // explicitly use flushing (submitting taichi's command list) here, which
+    // provides a semaphore between two command buffers.
+    runtime.flush();
+
+    frame += 1;
+    return true;
+  }
+  virtual void render() override final {
+    Renderer &renderer = F.renderer();
+    renderer.enqueue_graphics_task(*draw_points);
+    renderer.enqueue_graphics_task(*draw_collider_texture);
+  }
+};
+
+std::unique_ptr<App> create_app() {
+  return std::unique_ptr<App>(new ForceField);
+}
