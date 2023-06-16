97b57042f15a58ffdd173e9fc368131c3cf4e031
yucrazing <723284893@qq.com>
2023-06-16 10:25:45 +0800

    Add debugging example

---
+++ examples/debugging.cpp
@@ -0,0 +1,163 @@
+// avoid clang-format disorders headers
+// clang-format off
+#include <taichi/taichi.h>
+#include <soft2d/soft2d.h>
+#include "common.h"
+#include "globals.h"
+#include "emitter.h"
+#include "taichi/aot_demo/framework.hpp"
+#include <glm/gtc/matrix_transform.hpp>
+
+// clang-format on
+
+using namespace ti::aot_demo;
+using namespace std;
+
+constexpr int win_width = 800;
+constexpr int win_height = 800;
+constexpr float win_fov = 1.0 * win_width / win_height;
+
+struct Debugging : public App {
+
+  S2World world;
+  S2Trigger trigger;
+
+  std::unique_ptr<GraphicsTask> draw_points;
+  std::unique_ptr<GraphicsTask> draw_collider_texture;
+  std::unique_ptr<GraphicsTask> draw_trigger_texture;
+  std::unique_ptr<GraphicsTask> draw_mesh;
+  ti::NdArray<uint32_t> element_indices_;
+
+  ti::NdArray<float> x_;
+  ti::Texture collider_texture_;
+  ti::Texture trigger_texture_;
+
+  virtual AppConfig cfg() const override final {
+    AppConfig out{};
+    out.app_name = "Soft2D";
+    out.framebuffer_width = win_width;
+    out.framebuffer_height = win_height;
+    return out;
+  }
+
+  virtual void initialize() override final {
+    GraphicsRuntime &runtime = F.runtime();
+
+    // Soft2D initialization begins
+    S2WorldConfig config = default_world_config;
+    config.enable_debugging = true;
+    config.enable_world_query = true;
+    world = s2_create_world(TiArch::TI_ARCH_VULKAN, runtime, &config);
+
+    create_collider(world, make_kinematics({0.7f, 0.5f}),
+                    make_circle_shape(0.04));
+
+    create_collider(world, make_kinematics({0.5f, 0.5f}),
+                    make_ellipse_shape(0.04, 0.02));
+
+    create_collider(world, make_kinematics({0.3f, 0.5f}),
+                    make_capsule_shape(0.04, 0.02));
+
+    trigger = create_trigger(world, make_kinematics({0.5f, 0.3f}),
+                             make_box_shape(vec2(0.06f, 0.06f)));
+
+    // Soft2D initialization ends
+
+    // Renderer initialization begins
+    Renderer &renderer = F.renderer();
+    renderer.set_framebuffer_size(win_width, win_height);
+
+    x_ = runtime.allocate_vertex_buffer(config.max_allowed_particle_num, 2);
+    collider_texture_ = runtime.allocate_texture2d(
+        config.grid_resolution * config.fine_grid_scale,
+        config.grid_resolution * config.fine_grid_scale, TI_FORMAT_R32F,
+        TI_NULL_HANDLE);
+    trigger_texture_ = runtime.allocate_texture2d(
+        config.grid_resolution * config.fine_grid_scale,
+        config.grid_resolution * config.fine_grid_scale, TI_FORMAT_R32F,
+        TI_NULL_HANDLE);
+    element_indices_ =
+        runtime.allocate_index_buffer(config.max_allowed_element_num * 3, 1);
+
+    draw_points = runtime.draw_points(x_)
+                      .point_size(3.0f)
+                      .color(glm::vec3(1, 0.5, 0))
+                      .build();
+    draw_collider_texture = runtime.draw_texture(collider_texture_)
+                                .is_single_channel()
+                                .color(glm::vec3(0.2, 0.8, 0.0))
+                                .build();
+    draw_trigger_texture = runtime.draw_texture(trigger_texture_)
+                               .is_single_channel()
+                               .color(glm::vec3(0.85, 0.1, 0.0))
+                               .build();
+
+    // Set up orthogonal view
+    glm::mat4 model2world = glm::mat4(1.0f);
+    glm::mat4 camera2view =
+        glm::ortho(1.0, -1.0, 1.0 * win_fov, -1.0 * win_fov, -1000.0, 1000.0);
+    glm::mat4 world2camera = glm::lookAt(
+        glm::vec3(0.0, 0.0, 10), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0, -1, 0));
+    glm::mat4 world2view = camera2view * world2camera;
+
+    auto offset = s2_get_world_config(world).offset;
+    auto extent = s2_get_world_config(world).extent;
+    draw_mesh =
+        runtime
+            .draw_mesh(x_, element_indices_, glm::vec2(offset.x, offset.y),
+                       glm::vec2(extent.x, extent.y))
+            .model2world(model2world)
+            .world2view(world2view)
+            .color(glm::vec3(1.0, 0.5, 0.0))
+            .polygon_mode(VK_POLYGON_MODE_LINE)
+            .build();
+    // Renderer initialization ends
+  }
+  int frame = 0;
+  virtual bool update() override final {
+    GraphicsRuntime &runtime = F.runtime();
+
+    s2_step(world, 0.004);
+
+    // Export particle position data to the external buffer
+    TiNdArray particle_x;
+    s2_get_buffer(world, S2_BUFFER_NAME_PARTICLE_POSITION, &particle_x);
+    ndarray_data_copy(runtime.runtime(), x_.ndarray(), particle_x,
+                      sizeof(float) * 2 *
+                          s2_get_world_config(world).max_allowed_particle_num);
+
+    // Export collider and trigger buffers to texture
+    auto collider_tex = collider_texture_.texture();
+    auto trigger_tex = trigger_texture_.texture();
+    s2_export_buffer_to_texture(world, S2_BUFFER_NAME_FINE_GRID_COLLIDER_NUM,
+                                true, 0.8f, &collider_tex);
+    s2_export_buffer_to_texture(world, S2_BUFFER_NAME_FINE_GRID_TRIGGER_ID,
+                                true, 0.8f, &trigger_tex);
+
+    // Export element indices to the external buffer
+    TiNdArray element_indices_tmp;
+    s2_get_buffer(world, S2_BUFFER_NAME_ELEMENT_INDICES, &element_indices_tmp);
+    ndarray_data_copy(
+        runtime.runtime(), element_indices_.ndarray(), element_indices_tmp,
+        sizeof(int) * 3 * s2_get_world_config(world).max_allowed_element_num);
+
+    // Since taichi and renderer use different command buffers, we must
+    // explicitly use flushing (submitting taichi's command list) here, which
+    // provides a semaphore between two command buffers.
+    runtime.flush();
+
+    ++frame;
+    return true;
+  }
+  virtual void render() override final {
+    Renderer &renderer = F.renderer();
+    renderer.enqueue_graphics_task(*draw_mesh);
+    // renderer.enqueue_graphics_task(*draw_points);
+    renderer.enqueue_graphics_task(*draw_collider_texture);
+    renderer.enqueue_graphics_task(*draw_trigger_texture);
+  }
+};
+
+std::unique_ptr<App> create_app() {
+  return std::unique_ptr<App>(new Debugging);
+}
