f4ccf4ea0777f18ed6ba6b6f570268e392587fee
yucrazing <723284893@qq.com>
2023-06-26 21:09:44 +0800

    Update minimal example

--- tests/minimal.cpp
+++ tests/minimal.cpp
@@ -9,11 +9,11 @@ using namespace std;
 
 int main() {
 
+  // Create a runtime.
   TiArch arch = TiArch::TI_ARCH_VULKAN;
-  // Create a taichi runtime
   ti::Runtime runtime(arch);
 
-  // Specify the world configuration
+  // Specify the world configuration.
   S2WorldConfig config{};
   config.max_allowed_particle_num = 90000;
   config.max_allowed_body_num = 10000;
@@ -31,10 +31,10 @@ int main() {
   config.collision_penalty_force_scale_along_velocity_dir = 0.1f;
   config.fine_grid_scale = 4;
 
-  // Create a world
+  // Create a world.
   S2World world = s2_create_world(arch, runtime, &config);
 
-  // Simulate the world for 100 steps
+  // Simulate the world for 100 steps.
   float time_step = 2e-3f;
   for (int i = 0; i < 100; ++i) {
     s2_step(world, time_step);
